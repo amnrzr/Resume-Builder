858ae5a61a6c4869f0a0b35c3251df2e13053c9f
amnrzr <vipajsn@outlook.com>
2024-01-18 14:09:08 +0530

    Update card1.jsx

--- src/components/Details/card1.jsx
+++ src/components/Details/card1.jsx
@@ -1,42 +1,54 @@
+// Import the React library
 import React from "react";
 
-function Card1({ userData, education,dummyDetails }) {
+// Define the Card1 component
+function Card1({ userData, education, dummyDetails }) {
+  // Log user data and education information for debugging
   console.log("userData:", userData);
   console.log("education:", education);
-  let skillsset= userData.skills;
+
+  // Extract university and graduationYear based on education data or fallback to dummyDetails
   const university = education ? education[0]?.university : dummyDetails.university;
-  const graduationYear = education ? education[0]?.endYear : dummyDetails.startYear+'-'+dummyDetails.endYear;
-  
+  const graduationYear = education ? education[0]?.endYear : dummyDetails.startYear + '-' + dummyDetails.endYear;
+
+  // JSX structure for the Card1 component
   return (
     <div>
       <div id="resume1" className="clean-theme card-resume resume-container">
         <header className="header">
+          {/* Display user's full name and job title */}
           <h1>{userData.firstname} {userData.lastname}</h1>
           <p>Web Developer</p>
         </header>
+
         <div className="form-row">
+          {/* Contact and Objective section */}
           <section className="section wrap">
             <h4>Contact</h4>
             <p>Email: {userData.email}</p>
             <p>Phone: {userData.number}</p>
-            <p>Address:{userData.address}</p>
+            <p>Address: {userData.address}</p>
             <p>Objective: {userData.objective}</p>
           </section>
 
+          {/* Education section */}
           <section className="section2">
             <h4>Education</h4>
-            <p>University{university}</p>
+            <p>University: {university}</p>
             <p>Graduation Year: {graduationYear}</p>
           </section>
         </div>
+
+        {/* Work Experience section */}
         <section className="section3">
           <h4>Work Experience</h4>
           <p>Title: {userData.title}</p>
-          <p>Company:{userData.company}</p>
-          <p>Year:{userData.year}</p>
+          <p>Company: {userData.company}</p>
+          <p>Year: {userData.year}</p>
           <p>Responsibilities: {userData.responsibilities}</p>
         </section>
 
+        {/* Skills section */}
         <section className="section3">
           <h4>Skills</h4>
           <p>{userData.skill1}, {userData.skill2}</p>
@@ -47,4 +59,5 @@ function Card1({ userData, education,dummyDetails }) {
   );
 }
 
+// Export the Card1 component as the default export
 export default Card1;
